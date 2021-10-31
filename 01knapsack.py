6c49d079e262f3455d13b30fa790626d7ad5a221
rahulot <44512457+rahulot@users.noreply.github.com>
2021-10-31 23:22:25 +0530

    Update 01knapsack.py

--- 01knapsack.py
+++ 01knapsack.py
@@ -1,10 +1,3 @@
-'''def KnapSack(val, wt, n, W): 
-    dp = [0]*(W+1); 
-    for i in range(n): 
-        for j in range(W,wt[i],-1): 
-            dp[j] = max(dp[j] , val[i] + dp[j-wt[i]])
-    return dp[W]
-'''
 def knapSack(W, wt, val, n): 
     K = [[0 for x in range(W+1)] for x in range(n+1)] 
   
